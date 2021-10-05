f3cc893eff3055b5713912abecce0cd46c9e1646
Daniel Silveira <danielsilveiraluna07@gmail.com>
2021-10-05 14:00:26 -0300

    Add project to repository

---
+++ js/funcoes.js
@@ -0,0 +1,563 @@
+
+function Q1A() {
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q2A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q3A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa E")
+    }
+}
+
+function Q4A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q5A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q6A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q7A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa E")
+    }
+}
+
+function Q8A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q9A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q10A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q11A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q12A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q13A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q14A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q15A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q16A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q17A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q18A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q19A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q20A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q21A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q22A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q23A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q24A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q25A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q26A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q27A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa E")
+    }
+}
+
+function Q28A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa E")
+    }
+}
+
+function Q20A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opA.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa A")
+    }
+}
+
+function Q30A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q31A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opC.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa C")
+    }
+}
+
+function Q32A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opE.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa E")
+    }
+}
+
+function Q33A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function Q34A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opD.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa D")
+    }
+}
+
+function Q35A() {
+
+    var opA = document.getElementById("opA");
+    var opB = document.getElementById("opB");
+    var opC = document.getElementById("opC");
+    var opD = document.getElementById("opD");
+    var opE = document.getElementById("opE");
+
+    if (opB.checked==true) {
+        alert("VOCÊ ACERTOU!!")
+    }
+    else {
+        alert("VOCÊ ERROU, a opção correta era alternativa B")
+    }
+}
+
+function QEscrita() {
+    alert("Sua resposta vai ser avalida posteriormente!!")
+}
+
+function finalizar() {
+    alert("A função FINALIZAR, será implementada posteriormente!!")
+        alert("Obrigado por participar!!")
+}
